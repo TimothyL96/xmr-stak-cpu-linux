e8a4fa77e467293f6286fce05ea4649a1c168311
Timothy C. Lam <geroginalam@hotmail.com>
2017-12-17 02:38:23 +0800

    cd

--- run.sh
+++ run.sh
@@ -7,4 +7,4 @@
 apt update
 apt -y install unzip subversion libhwloc-dev libmicrohttpd-dev
 svn checkout https://github.com/ttimt/xmr-stak-cpu-linux/trunk/xmr-stak-cpu
-cd xmr-stak-cpu
+#cd $HOME/xmr-stak-cpu
