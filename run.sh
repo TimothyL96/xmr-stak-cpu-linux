9e14d34d7bf9e58f94680d6ab5d215e7b2115e8d
Timothy C. Lam <geroginalam@hotmail.com>
2017-12-17 04:02:44 +0800

    removed unzip and commented cd

--- run.sh
+++ run.sh
@@ -5,6 +5,6 @@
 
 
 apt update
-apt -y install unzip subversion libhwloc-dev libmicrohttpd-dev
+apt -y install subversion libhwloc-dev libmicrohttpd-dev
 svn checkout https://github.com/ttimt/xmr-stak-cpu-linux/trunk/xmr-stak-cpu
-cd ./xmr-stak-cpu
+#cd $HOME/xmr-stak-cpu
