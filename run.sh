fa67ee38bcfd4fc39a4cd9661d15a3c93b1df77e
Timothy C. Lam <geroginalam@hotmail.com>
2017-12-16 22:51:01 +0800

    auto install dependency install

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Install 0 donation xmr-stak-cpu linux
+# auto install dependency install
+
+
+apt install unzip libhwloc-dev libmicrohttpd-dev
+wget https://github.com/ttimt/xmr-stak-cpu-linux/archive/master.zip
+unzip master.zip
+clear
+cd xmr-stak-cpu-linux-master
